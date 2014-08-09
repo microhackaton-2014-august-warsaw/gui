59f0a2992476dacf911e39eb216a204267ecb21e
szimano <szimano@szimano.org>
2014-08-09 19:11:32 +0200

    remove peasant's version

--- src/main/web/app/scripts/controllers/main.js
+++ src/main/web/app/scripts/controllers/main.js
@@ -42,6 +42,7 @@ angular.module('youShouldRememberMeUiApp')
 
             if ($routeParams.id) {
                 $scope.personToMatch = link.post({'url': $routeParams.id}, '{}');
+                delete $scope.personToMatch['version'];
             }
 
             $scope.link = '';
